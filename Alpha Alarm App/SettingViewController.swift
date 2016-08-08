63e23144192b3ba69040b6e9e3e53862479587ea
vickywang410 <vickywang410@gmail.com>
2016-08-08 17:16:32 -0400

    Initial Commit

---
+++ Alpha Alarm App/SettingViewController.swift
@@ -0,0 +1,21 @@
+ 
+import UIKit
+
+class SettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print ("Setting Page Loaded")
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+
+}
